68b9d22f8efbb4294567ebe2f41703f8851d356b
preetbhullar90 <62619558+preetbhullar90@users.noreply.github.com>
2022-01-29 00:56:49 +0000

    add form

--- reservation/views.py
+++ reservation/views.py
@@ -6,12 +6,16 @@ from .forms import ReserveTableForm
 from reservation.models import Reservation
 
 def reserve_table(request):
-    reserve_form = ReserveTableForm
+    reserve_form = ReserveTableForm()
+    
     if request.method == 'POST':
         reserve_form = ReserveTableForm(request.POST)
+        
         if reserve_form.is_valid():
             reserve_form.save() 
+            
     context = {'form': reserve_form }
+    
     return render(request, 'Reservation/reservation.html', context)
             
     
